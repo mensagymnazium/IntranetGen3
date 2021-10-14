c950b535e650419b43f7dd84c9d7031b0f2eba82
BuildTools <unconfigured@null.spigotmc.org>
2021-10-14 16:11:49 +0200

    #10 Created an empty test class for the LoginDisplay in the sidebar footer

---
+++ Web.Client.Tests/LoginDisplayTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MensaGymnazium.IntranetGen3.Web.Client.Tests
+{
+	[TestClass]
+	public class LoginDisplayTests
+	{
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_NameIsNull()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_NameIsOneWord()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_NameIsTwoWords()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_NameIsThreeWords()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_NameIsFourWords()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_EmailIsOneWord()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_EmailIsTwoWords()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_EmailIsThreeWords()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+		[TestMethod]
+		public void LoginDisplay_NameToInitials_EmailIsFourWords()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+	}
+}
