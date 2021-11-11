027b33434b5436ef772a7c00e64e6ba7441a8127
Robert Haken <haken@havit.cz>
2021-11-11 12:56:36 +0100

    Project template cleanup (Contacts)

--- Web.Client/Services/DataStores/IContactReferenceDataStore.cs
+++
@@ -1,9 +0,0 @@
-﻿using Havit.Blazor.Components.Web.Services.DataStores;
-using MensaGymnazium.IntranetGen3.Contracts.Crm;
-
-namespace MensaGymnazium.IntranetGen3.Web.Client.Services.DataStores
-{
-	public interface IContactReferenceDataStore : IDictionaryStaticDataStore<int, ContactReferenceVM>
-	{
-	}
-}
