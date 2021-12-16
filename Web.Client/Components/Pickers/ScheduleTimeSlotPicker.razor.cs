b631def6d4d479eacf236946a755b68a7dfaa141
Robert Haken <haken@havit.cz>
2021-12-16 13:45:11 +0100

    ScheduleTimeSlotPicker code-behind

---
+++ Web.Client/Components/Pickers/ScheduleTimeSlotPicker.razor.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using Blazored.FluentValidation;
+using MensaGymnazium.IntranetGen3.Contracts;
+using MensaGymnazium.IntranetGen3.Web.Client;
+using MensaGymnazium.IntranetGen3.Web.Client.Components;
+using MensaGymnazium.IntranetGen3.Web.Client.Components.Pickers;
+using MensaGymnazium.IntranetGen3.Web.Client.Shared;
+using MensaGymnazium.IntranetGen3.Primitives;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
+using System.Globalization;
+using System.ComponentModel;
+
+namespace MensaGymnazium.IntranetGen3.Web.Client.Components.Pickers
+{
+	public partial class ScheduleTimeSlotPicker
+	{
+		[Parameter] public DayOfWeek? Day { get; set; }
+
+		[Parameter] public EventCallback<DayOfWeek?> DayChanged { get; set; }
+
+		[Parameter] public ScheduleSlotInDay? Slot { get; set; }
+
+		[Parameter] public EventCallback<ScheduleSlotInDay?> SlotChanged { get; set; }
+
+		private static DateTimeFormatInfo DateTimeFormat => new CultureInfo("cs-CZ").DateTimeFormat;
+	}
+}
