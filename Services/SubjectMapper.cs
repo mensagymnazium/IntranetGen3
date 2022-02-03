bc5cf7cdbdb34339d3da2ac53e271c099b516089
Richard Dobíšek <richard.dobisek@mensagymnazium.cz>
2022-02-03 15:47:25 +0100

    Přidání a editování subjectů

---
+++ Services/SubjectMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Diagnostics.Contracts;
+using Havit.Extensions.DependencyInjection.Abstractions;
+using MensaGymnazium.IntranetGen3.Contracts;
+using MensaGymnazium.IntranetGen3.Model;
+
+namespace MensaGymnazium.IntranetGen3.Services
+{
+	[Service]
+	public class SubjectMapper : ISubjectMapper
+	{
+		public void MapFromSubjectDto(SubjectDto subjectDto, Subject subject)
+		{
+			Contract.Requires<ArgumentNullException>(subjectDto is not null);
+			Contract.Requires<ArgumentNullException>(subject is not null);
+
+			subject.Name = subjectDto.Name;
+		}
+
+		public SubjectDto MapToSubjectDto(Subject subject)
+		{
+			Contract.Requires<ArgumentNullException>(subject is not null);
+
+			return new SubjectDto
+			{
+				Id = subject.Id,
+				Name = subject.Name
+
+			};
+		}
+	}
+}
