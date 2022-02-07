0e1198d5793d2ba17c430c43b0054980c30b2f32
Robert Haken <haken@havit.cz>
2022-02-07 01:58:42 +0100

    DataSeedingTests - DEBUG only (whole test-class)

--- IntegrationTests/DataLayer/Seeds/DataSeedingTests.cs
+++ IntegrationTests/DataLayer/Seeds/DataSeedingTests.cs
@@ -1,23 +1,18 @@
-﻿using System.Linq;
-using Havit.Data.Patterns.DataSeeds;
+﻿using Havit.Data.Patterns.DataSeeds;
 using MensaGymnazium.IntranetGen3.DataLayer.Seeds.Core;
-using MensaGymnazium.IntranetGen3.Model.Common;
-using Havit.Data.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Extensions.DependencyInjection;
-using Havit.Services.Caching;
 using MensaGymnazium.IntranetGen3.TestHelpers;
 
 namespace MensaGymnazium.IntranetGen3.IntegrationTests.DataLayer.Seeds
 {
+#if DEBUG
 	[TestClass]
 	public class DataSeedingTests : IntegrationTestBase
 	{
 		protected override bool UseLocalDb => true;
 		protected override bool DeleteDbData => true; // default, but to be sure :D
 
-#if DEBUG
 		[TestMethod]
 		public void DataSeeds_CoreProfile()
 		{
@@ -30,6 +25,6 @@ namespace MensaGymnazium.IntranetGen3.IntegrationTests.DataLayer.Seeds
 			// assert
 			// no exception
 		}
-#endif
 	}
+#endif
 }
