afb57ef953959ef1c2327a9242415745bc094883
Robert Haken <haken@havit.cz>
2022-02-10 13:13:21 +0100

    StudentSubjectRegistration - model consolidation - Created, Deleted

---
+++ Entity/Migrations/20220210121233_StudentSubjectRegistrationCreatedDeleted.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MensaGymnazium.IntranetGen3.Entity.Migrations
+{
+    public partial class StudentSubjectRegistrationCreatedDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedTime",
+                table: "StudentSubjectRegistration",
+                newName: "Created");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Deleted",
+                table: "StudentSubjectRegistration",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "StudentSubjectRegistration");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "StudentSubjectRegistration",
+                newName: "CreatedTime");
+        }
+    }
+}
