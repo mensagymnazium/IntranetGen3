5e968f223f98a6f1351f530f0d21d5b97f544a42
Robert Haken <haken@havit.cz>
2022-03-10 12:30:53 +0100

    cleanup

--- Contracts/SubjectListItemDto.cs
+++ Contracts/SubjectListItemDto.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MensaGymnazium.IntranetGen3.Primitives;
 
 namespace MensaGymnazium.IntranetGen3.Contracts
 {
@@ -13,6 +9,6 @@ namespace MensaGymnazium.IntranetGen3.Contracts
 		public int CategoryId { get; set; }
 		public int Capacity { get; set; }
 		public int SubjectTypeId { get; set; }
-		public Primitives.ScheduleSlotInDay ScheduleSlotInDay { get; set; }
+		public ScheduleSlotInDay ScheduleSlotInDay { get; set; }
 	}
 }
