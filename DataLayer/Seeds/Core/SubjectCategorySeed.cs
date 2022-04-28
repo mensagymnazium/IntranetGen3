7bb1145759e3f22af0fc6db67310786dc7297d69
Martin Truksa <martin.truksa@mensagymnazium.cz>
2022-04-28 16:14:40 +0200

    SubjectSeed, SubjectCategorySeed a SubjectTypeSeed, TeacherSeed dodělám

--- DataLayer/Seeds/Core/SubjectCategorySeed.cs
+++ DataLayer/Seeds/Core/SubjectCategorySeed.cs
@@ -8,10 +8,11 @@ public class SubjectCategorySeed : DataSeed<CoreProfile>
 	{
 		var data = new[]
 		{
-			new SubjectCategory() { Id = (int)SubjectCategory.Entry.GraduationSeminar, Name = "maturitní seminář" },
+			new SubjectCategory() { Id = (int)SubjectCategory.Entry.GraduationSeminar, Name = "přednáška" },
+			/*new SubjectCategory() { Id = (int)SubjectCategory.Entry.GraduationSeminar, Name = "maturitní seminář" },
 			new SubjectCategory() { Id = (int)SubjectCategory.Entry.ExtensionSeminar, Name = "nadstavbový seminář" },
 			new SubjectCategory() { Id = (int)SubjectCategory.Entry.SpecialisationSeminar, Name = "specializační seminář" },
-			new SubjectCategory() { Id = (int)SubjectCategory.Entry.ForeignLanguage, Name = "cizí jazyk" },
+			new SubjectCategory() { Id = (int)SubjectCategory.Entry.ForeignLanguage, Name = "cizí jazyk" },*/
 		};
 
 		Seed(For(data).PairBy(st => st.Id));
--- DataLayer/Seeds/Core/SubjectTypeSeed.cs
+++ DataLayer/Seeds/Core/SubjectTypeSeed.cs
@@ -8,14 +8,15 @@ public class SubjectTypeSeed : DataSeed<CoreProfile>
 	{
 		var data = new[]
 		{
-			new SubjectType() { Name = "jazyk a jazyková komunikace", Id = (int)SubjectType.Entry.LanguageCommunication },
+			new SubjectType() { Name = "Placeholder type", Id = (int)SubjectType.Entry.Informatics },
+			/*new SubjectType() { Name = "jazyk a jazyková komunikace", Id = (int)SubjectType.Entry.LanguageCommunication },
 			new SubjectType() { Name = "matematika a její aplikace", Id= (int)SubjectType.Entry.MathApplication },
 			new SubjectType() { Name = "informační a komunikační technologie", Id = (int)SubjectType.Entry.Informatics },
 			new SubjectType() { Name = "člověk a společnost", Id = (int)SubjectType.Entry.HumanSociety },
 			new SubjectType() { Name = "člověk a příroda", Id = (int)SubjectType.Entry.HumanNature },
 			new SubjectType() { Name = "umění a kultura", Id = (int)SubjectType.Entry.ArtCulture },
 			new SubjectType() { Name = "člověk a zdraví", Id = (int)SubjectType.Entry.HumanHealth },
-			new SubjectType() { Name = "člověk a svět práce", Id = (int)SubjectType.Entry.HumanWork },
+			new SubjectType() { Name = "člověk a svět práce", Id = (int)SubjectType.Entry.HumanWork },*/
 		};
 
 		Seed(For(data).PairBy(st => st.Id));
