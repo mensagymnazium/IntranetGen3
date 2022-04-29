1b00a84ccbd4314104bff21b46f04890d631a2b8
Benjamin Swart <Benjaminswart@email.cz>
2022-04-29 11:45:00 +0200

    Fix collision detection
    Closes #87

--- DataLayer/Repositories/IStudentSubjectRegistrationRepository.cs
+++ DataLayer/Repositories/IStudentSubjectRegistrationRepository.cs
@@ -1,8 +1,13 @@
 ﻿using MensaGymnazium.IntranetGen3.Model;
+using MensaGymnazium.IntranetGen3.Primitives;
 
 namespace MensaGymnazium.IntranetGen3.DataLayer.Repositories;
 
 public partial interface IStudentSubjectRegistrationRepository
 {
 	Task<List<StudentSubjectRegistration>> GetBySubjectAsync(int id, CancellationToken cancellationToken = default);
+
+	public Task<long> CountBySubjectAndTypeAsync(int subjectId, StudentRegistrationType type, CancellationToken cancellationToken = default);
+
+	public Task<List<StudentSubjectRegistration>> GetByStudentAndTimeAsync(int studentId, DayOfWeek day, ScheduleSlotInDay slot, CancellationToken cancellationToken = default);
 }
