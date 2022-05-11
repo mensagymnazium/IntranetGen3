f6450e8bcb574721fc894446472b14439a7ab052
Robert Haken <haken@havit.cz>
2022-05-11 17:33:03 +0200

    Job warning resolution

--- Services/Jobs/EmptyJob.cs
+++ Services/Jobs/EmptyJob.cs
@@ -12,12 +12,14 @@ public class EmptyJob : IEmptyJob
 		this.logger = logger;
 	}
 
-	public async Task ExecuteAsync(CancellationToken cancellationToken)
+	public Task ExecuteAsync(CancellationToken cancellationToken)
 	{
 		logger.LogInformation("Begin: EmptyJob");
 
 		// TODO await Task.Delay(5000, cancellationToken);
 
 		logger.LogInformation("End: EmptyJob");
+
+		return Task.CompletedTask;
 	}
 }
