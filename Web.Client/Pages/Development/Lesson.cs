39ea4ec52356a5e6fb8a348cb0eec8f6e796f1f6
Robert Haken <haken@havit.cz>
2022-06-03 10:09:42 +0200

    StyleCop fixes

---
+++ Web.Client/Pages/Development/Lesson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MensaGymnazium.IntranetGen3.Web.Client.Pages.Development
+{
+	public class Lesson
+	{
+		public DayOfWeek DayOfWeek { get; set; }
+		public string Name { get; set; }
+		public string Block { get; set; }
+		public Lesson(string name, string block, DayOfWeek dayOfWeeks)
+		{
+			this.DayOfWeek = dayOfWeeks;
+			this.Name = name;
+			this.Block = block;
+		}
+	}
+}
