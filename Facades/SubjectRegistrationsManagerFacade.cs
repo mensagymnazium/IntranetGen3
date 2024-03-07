a2159450162ed7058e5fdd361d57eff6ecb5cb34
Jonáš Holub <h.kolombooo@gmail.com>
2024-03-07 10:28:57 +0100

    Uprava nesprávně zadaného datumu uzávěrky

--- Facades/SubjectRegistrationsManagerFacade.cs
+++ Facades/SubjectRegistrationsManagerFacade.cs
@@ -221,7 +221,7 @@ public class SubjectRegistrationsManagerFacade : ISubjectRegistrationsManagerFac
 
 	private void VerifyRegistrationChangesAllowedToStudents()
 	{
-		if (timeService.GetCurrentDate() > new DateTime(2024, 3, 12))
+		if (timeService.GetCurrentDate() > new DateTime(2024, 3, 13))
 		{
 			throw new OperationFailedException("Registrace jsou uzavřeny, kontaktujte organizátory.");
 		}
