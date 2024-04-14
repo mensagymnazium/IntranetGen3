9fc63f602fbb7d05cf3e1380ab00f7c82579726c
Xopabyteh <70832276+Xopabyteh@users.noreply.github.com>
2024-04-14 14:27:07 +0200

    Add GradeRegistrationCriteria. #131 #132 #133

---
+++ DataLayer/Seeds/Core/GradeRegistrationCriteriaSeed.cs
@@ -0,0 +1,24 @@
+﻿using MensaGymnazium.IntranetGen3.Model;
+using MensaGymnazium.IntranetGen3.Primitives;
+
+namespace MensaGymnazium.IntranetGen3.DataLayer.Seeds.Core;
+
+public class GradeSeedRegistrationCriteria : DataSeed<CoreProfile>
+{
+	public override void SeedData()
+	{
+		var data = new[]
+		{
+			new GradeRegistrationCriteria() { Id = (int) GradeEntry.Prima},
+			new GradeRegistrationCriteria() { Id = (int) GradeEntry.Sekunda},
+			new GradeRegistrationCriteria() { Id = (int) GradeEntry.Tercie},
+			new GradeRegistrationCriteria() { Id = (int) GradeEntry.Kvarta},
+			new GradeRegistrationCriteria() { Id = (int) GradeEntry.Kvinta},
+			new GradeRegistrationCriteria() { Id = (int) GradeEntry.Sexta},
+			new GradeRegistrationCriteria() { Id = (int) GradeEntry.Septima},
+			new GradeRegistrationCriteria() { Id = (int) GradeEntry.Oktava},
+		};
+
+		Seed(For(data).PairBy(grade => grade.Id)); // TODO WithoutUpdate nebo bez AadGroupId? 
+	}
+}
