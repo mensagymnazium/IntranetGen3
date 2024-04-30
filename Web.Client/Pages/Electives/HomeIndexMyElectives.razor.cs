e6cf4a9d86f2820b727acecf094d38477cd637f4
kleplejklep <max.jarsky@gmail.com>
2024-04-30 15:55:02 +0200

    Home page shouldnt crash for teachers, (still wont load the page itsself - but nothing useful is there for teachers)

--- Web.Client/Pages/Electives/HomeIndexMyElectives.razor.cs
+++ Web.Client/Pages/Electives/HomeIndexMyElectives.razor.cs
@@ -1,17 +1,26 @@
 ﻿using Havit.Collections;
 using MensaGymnazium.IntranetGen3.Contracts;
+using MensaGymnazium.IntranetGen3.Web.Client.Services;
 
 namespace MensaGymnazium.IntranetGen3.Web.Client.Pages.Electives;
 
 public partial class HomeIndexMyElectives
 {
 	[Inject] protected ISubjectRegistrationProgressValidationFacade SubjectRegistrationProgressValidationFacade { get; set; }
+	[Inject] protected IClientAuthService ClientAuthService { get; set; }
 
 	private StudentRegistrationProgressDto studentsProgress;
+
 
 	protected override async Task OnAfterRenderAsync(bool firstRender)
 	{
 		if (!firstRender)
+		{
+			return;
+		}
+
+		var user = await ClientAuthService.GetCurrentClaimsPrincipal();
+		if (!user.IsInRole("Student"))
 		{
 			return;
 		}
