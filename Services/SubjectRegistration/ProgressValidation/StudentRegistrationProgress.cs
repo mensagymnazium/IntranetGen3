1017bf7390a5ccd60a4bcadabf4277e0e225017c
Xopabyteh <70832276+Xopabyteh@users.noreply.github.com>
2024-05-01 11:27:08 +0200

    StudentRegistrationProgress - record - primary constructor vs. properties (code-review) #159

--- Services/SubjectRegistration/ProgressValidation/StudentRegistrationProgress.cs
+++ Services/SubjectRegistration/ProgressValidation/StudentRegistrationProgress.cs
@@ -1,27 +1,26 @@
 ﻿namespace MensaGymnazium.IntranetGen3.Services.SubjectRegistration.ProgressValidation;
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="IsRegistrationValid">
+/// When true, the student has registered all of his subjects correctly,
+/// meaning he meets all the signing criteria, or a combination of them,
+/// that results in a valid registration
+/// </param>
+/// <param name="DonatedHoursProgress">
+/// Progress of the rule about weekly donated hours
+/// </param>
+/// <param name="CsOrCpRegistrationProgress">
+/// Progress of the rule:
+/// 'N musí být ze vzdělávacích oblastí „Člověk a
+/// společnost“ či „Člověk a příroda“'
+/// </param>
+/// <param name="LanguageRegistrationProgress">
+/// Progress of the rule about having a language
+/// </param>
 public record StudentRegistrationProgress(
 	bool IsRegistrationValid,
 	StudentDonatedHoursProgress DonatedHoursProgress,
 	StudentCsOrCpRegistrationProgress CsOrCpRegistrationProgress,
-	StudentLanguageRegistrationProgress LanguageRegistrationProgress)
-{
-	/// <summary>
-	/// When true, the student has registered all of his subjects correctly,
-	/// meaning he meets all the signing criteria, or a combination of them,
-	/// that results in a valid registration
-	/// </summary>
-	public bool IsRegistrationValid { get; init; } = IsRegistrationValid;
-
-	/// <summary>
-	/// Progress of the rule:
-	/// 'N musí být ze vzdělávacích oblastí „Člověk a
-	/// společnost“ či „Člověk a příroda“'
-	/// </summary>
-	public StudentCsOrCpRegistrationProgress CsOrCpRegistrationProgress { get; init; } = CsOrCpRegistrationProgress;
-
-	/// <summary>
-	/// Progress of the rule about weekly donated hours
-	/// </summary>
-	public StudentDonatedHoursProgress DonatedHoursProgress { get; init; } = DonatedHoursProgress;
-}
+	StudentLanguageRegistrationProgress LanguageRegistrationProgress);
