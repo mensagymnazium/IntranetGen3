61f7a976d14a9ebf27627b763b43a3493dd9523b
Xopabyteh <70832276+Xopabyteh@users.noreply.github.com>
2024-05-03 18:28:24 +0200

    GradeRegistrationCriteria - Vyrobit editační komponentu #147

---
+++ Contracts/GradeRegistrationCriteriaDto.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace MensaGymnazium.IntranetGen3.Contracts;
+public record GradeRegistrationCriteriaDto
+{
+	public int GradeId { get; set; }
+	public int RequiredTotalAmountOfDonatedHoursExcludingLanguage { get; set; }
+	public bool RequiresCspOrCpValidation { get; set; }
+	public int RequiredAmountOfDonatedHoursInAreaCspOrCp { get; set; }
+	public bool RequiresForeignLanguage { get; set; }
+	public bool CanUseForeignLanguageInsteadOfDonatedHours { get; set; }
+
+	public class GradeRegistrationCriteriaValidator : AbstractValidator<GradeRegistrationCriteriaDto>
+	{
+		public GradeRegistrationCriteriaValidator()
+		{
+			RuleFor(c => c.RequiredTotalAmountOfDonatedHoursExcludingLanguage)
+				.GreaterThanOrEqualTo(0)
+				.WithName("Hodiny v rozvrhu (kromě jazyků)");
+
+			RuleFor(c => c.RequiredAmountOfDonatedHoursInAreaCspOrCp)
+				.GreaterThanOrEqualTo(0)
+				.WithName("Hodiny v oblasti ČSP/ČP");
+
+			RuleFor(c => c.CanUseForeignLanguageInsteadOfDonatedHours)
+				.NotEqual(true)
+				.When(c => c.RequiresForeignLanguage)
+				.WithMessage("Ročník nemůže vyžadovat jazyk a zároveň ho využít namísto hodin v rozvrhu");
+		}
+	}
+}
--- Facades/GradeFacade.cs
+++ Facades/GradeFacade.cs
@@ -1,5 +1,6 @@
 ﻿using MensaGymnazium.IntranetGen3.Contracts;
 using MensaGymnazium.IntranetGen3.DataLayer.Repositories;
+using MensaGymnazium.IntranetGen3.Model;
 
 namespace MensaGymnazium.IntranetGen3.Facades;
 
@@ -8,10 +9,12 @@ namespace MensaGymnazium.IntranetGen3.Facades;
 public class GradeFacade : IGradeFacade
 {
 	private readonly IGradeRepository gradeRepository;
+	private readonly IUnitOfWork unitOfWork;
 
-	public GradeFacade(IGradeRepository gradeRepository)
+	public GradeFacade(IGradeRepository gradeRepository, IUnitOfWork unitOfWork)
 	{
 		this.gradeRepository = gradeRepository;
+		this.unitOfWork = unitOfWork;
 	}
 
 	public async Task<List<GradeDto>> GetAllGradesAsync(CancellationToken cancellationToken = default)
@@ -26,5 +29,60 @@ public class GradeFacade : IGradeFacade
 				Name = g.Name
 			})
 			.ToList();
+	}
+
+	public async Task<List<GradeRegistrationCriteriaDto>> GetGradeRegistrationCriteriasAsync(CancellationToken cancellationToken = default)
+	{
+		// Only ever get 8 elements, so we can load all to memory
+		var grades = await gradeRepository.GetAllAsync(cancellationToken);
+
+		return grades
+			.Select(g => new GradeRegistrationCriteriaDto()
+			{
+				GradeId =
+					g.Id,
+
+				CanUseForeignLanguageInsteadOfDonatedHours =
+					g.RegistrationCriteria.CanUseForeignLanguageInsteadOfDonatedHours,
+
+				RequiredAmountOfDonatedHoursInAreaCspOrCp =
+					g.RegistrationCriteria.RequiredAmountOfDonatedHoursInAreaCspOrCp,
+
+				RequiredTotalAmountOfDonatedHoursExcludingLanguage =
+					g.RegistrationCriteria.RequiredTotalAmountOfDonatedHoursExcludingLanguage,
+
+				RequiresCspOrCpValidation =
+					g.RegistrationCriteria.RequiresCspOrCpValidation,
+
+				RequiresForeignLanguage =
+					g.RegistrationCriteria.RequiresForeginLanguage
+			})
+			.ToList();
+	}
+
+	public async Task UpdateGradeRegistrationCriteriaAsync(GradeRegistrationCriteriaDto model, CancellationToken cancellation = default)
+	{
+		Contract.Requires<ArgumentNullException>(model is not null);
+		if (model.CanUseForeignLanguageInsteadOfDonatedHours && model.RequiresForeignLanguage)
+		{
+			// Xopa: Maybe app logic is leaking and this should be a service?
+			throw new InvalidOperationException("Ročník nemůže vyžadovat jazyk a zároveň ho využít namísto hodin v rozvrhu");
+		}
+
+		var grade = await gradeRepository.GetObjectAsync(model.GradeId, cancellation);
+
+		MapRegistrationCriteriaFromDTO(model, grade.RegistrationCriteria);
+
+		unitOfWork.AddForUpdate(grade);
+		await unitOfWork.CommitAsync(cancellation);
+	}
+
+	private void MapRegistrationCriteriaFromDTO(GradeRegistrationCriteriaDto dto, GradeRegistrationCriteria criteria)
+	{
+		criteria.CanUseForeignLanguageInsteadOfDonatedHours = dto.CanUseForeignLanguageInsteadOfDonatedHours;
+		criteria.RequiredAmountOfDonatedHoursInAreaCspOrCp = dto.RequiredAmountOfDonatedHoursInAreaCspOrCp;
+		criteria.RequiredTotalAmountOfDonatedHoursExcludingLanguage = dto.RequiredTotalAmountOfDonatedHoursExcludingLanguage;
+		criteria.RequiresCspOrCpValidation = dto.RequiresCspOrCpValidation;
+		criteria.RequiresForeginLanguage = dto.RequiresForeignLanguage;
 	}
 }
