1fdea33c319d9934d9bf9653df8d3652ecd3d7f1
Robert Haken <haken@havit.cz>
2024-05-07 14:47:52 +0200

    DB migrartion fix

---
+++ Entity/Migrations/20240507124725_DotatedHoursRenaming.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MensaGymnazium.IntranetGen3.Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class DotatedHoursRenaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RequiredTotalAmountOfDonatedHoursExcludingLanguage",
+                table: "GradeRegistrationCriteria",
+                newName: "RequiredTotalAmountOfHoursPerWeekExcludingLanguage");
+
+            migrationBuilder.RenameColumn(
+                name: "RequiredAmountOfDonatedHoursInAreaCspOrCp",
+                table: "GradeRegistrationCriteria",
+                newName: "RequiredAmountOfHoursPerWeekInAreaCspOrCp");
+
+            migrationBuilder.RenameColumn(
+                name: "CanUseForeignLanguageInsteadOfDonatedHours",
+                table: "GradeRegistrationCriteria",
+                newName: "CanUseForeignLanguageInsteadOfHoursPerWeek");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RequiredTotalAmountOfHoursPerWeekExcludingLanguage",
+                table: "GradeRegistrationCriteria",
+                newName: "RequiredTotalAmountOfDonatedHoursExcludingLanguage");
+
+            migrationBuilder.RenameColumn(
+                name: "RequiredAmountOfHoursPerWeekInAreaCspOrCp",
+                table: "GradeRegistrationCriteria",
+                newName: "RequiredAmountOfDonatedHoursInAreaCspOrCp");
+
+            migrationBuilder.RenameColumn(
+                name: "CanUseForeignLanguageInsteadOfHoursPerWeek",
+                table: "GradeRegistrationCriteria",
+                newName: "CanUseForeignLanguageInsteadOfDonatedHours");
+        }
+    }
+}
