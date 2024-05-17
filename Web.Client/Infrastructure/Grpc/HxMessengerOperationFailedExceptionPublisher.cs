bf8d0f7754d5a4135afb1ec7064d9db4258ec8a2
Slavek592 <71459428+slavek592@users.noreply.github.com>
2024-05-17 11:06:00 +0200

    refactoring 2

--- Web.Client/Infrastructure/Grpc/HxMessengerOperationFailedExceptionPublisher.cs
+++ Web.Client/Infrastructure/Grpc/HxMessengerOperationFailedExceptionPublisher.cs
@@ -4,16 +4,16 @@ namespace MensaGymnazium.IntranetGen3.Web.Client.Infrastructure.Grpc;
 
 public class HxMessengerOperationFailedExceptionGrpcClientListener : IOperationFailedExceptionGrpcClientListener
 {
-	private readonly IHxMessengerService messenger;
+	private readonly IHxMessengerService _messenger;
 
 	public HxMessengerOperationFailedExceptionGrpcClientListener(IHxMessengerService messenger)
 	{
-		this.messenger = messenger;
+		_messenger = messenger;
 	}
 
 	public Task ProcessAsync(string errorMessage)
 	{
-		messenger.AddError("Něco se nepovedlo", errorMessage);
+		_messenger.AddError("Něco se nepovedlo", errorMessage);
 
 		return Task.CompletedTask;
 	}
